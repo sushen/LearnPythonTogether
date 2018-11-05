4d1d218713ba4a24cd0ebb617366e9d2c2028412
Nahid <bappyhasan1994247@gmail.com>
2018-11-05 08:28:17 -0800

    i am updating my index

---
+++ HowToBuildAnIdea.py
@@ -0,0 +1,80 @@
+# Ita all about who consciously you think.
+# In my opinions if you think 2 days of a idea you will bring a sophisticate idea to world.
+
+# Lest Understand if you do a 8 hours job in a corporate house
+# and you do 5 days in a week basic with lifetime.
+# lets say you join this job in you 26 years age and
+# you will retired in your 56 years old how many hours you expend in your work.
+# Its you achievement of lifetime. it ant it?
+
+# Lets bring the mathematics.
+
+Daily_Hours = 8
+Per_Week_Days = 5
+Total_Hours_in_a_week = Daily_Hours * Per_Week_Days
+
+#print(Total_Hours_in_a_week)
+
+Total_Hours_in_a_Month = Total_Hours_in_a_week * 4
+
+#print(Total_Hours_in_a_Month)
+
+Total_Hours_in_a_Years = Total_Hours_in_a_Month * 12
+
+#print(Total_Hours_in_a_Years)
+
+How_Long_Years_You_will_Work = 56 - 26
+
+#print(How_Long_Years_You_will_Work)
+
+How_Long_Hours_You_Have_to_Work = How_Long_Years_You_will_Work * Total_Hours_in_a_Years
+
+#print(How_Long_Hours_You_Have_to_Work)
+
+You_will_spend_How_Much_day = How_Long_Hours_You_Have_to_Work / 24
+
+#print(You_will_spend_How_Much_day)
+
+You_will_spend_How_Much_Month = You_will_spend_How_Much_day /30
+
+#print(You_will_spend_How_Much_Month)
+
+You_will_spend_How_Much_Years = You_will_spend_How_Much_Month/12
+
+print("Total " + str(You_will_spend_How_Much_Years) + " years you have to work if you do a 8 hours daily job.")
+
+
+# Now People are saying its take 2 to 3 years to stabilise a business idea.
+# You must understand what is Idea?
+# Idea is some thing can solve problem.
+# Idea comes your mind every time any time.
+# Sometime you write it down and most of the time its go way and its naver come back.
+# People hardly use there own Idea.
+# Some People really do and they stack in there Own Idea and 2 to 3 years pass and they make there family happy.
+# In my opinion you can hardly think 10 mints and extend your idea and within 3 years you loos full motivation.
+# So What will be the Mathematics of that?
+
+Total_Thinking_of_a_day = 10
+Total_Thinking_of_a_Week = Total_Thinking_of_a_day * 5
+#print(Total_Thinking_of_a_Week)
+Total_Thinking_of_a_Month = Total_Thinking_of_a_Week * 4
+#print(Total_Thinking_of_a_Month)
+Total_Thinking_of_a_Years = Total_Thinking_of_a_Month * 12
+#print(Total_Thinking_of_a_Years)
+How_Many_Hours_You_think = Total_Thinking_of_a_Years / 60
+#print(How_Many_Hours_You_think)
+How_Many_Days_You_think = How_Many_Hours_You_think /24
+print("Total "+ str(How_Many_Days_You_think) + " Days to take bring a full face problem solver Idea.")
+
+# So In my opinion with deep contemplating if you bring a problem its take not more than 2 days to solve.
+# But I will want tell you some more. The Moon Science. Every mature woman will understand it more than man.
+# Every Full Moon Our Body become full face just like the ocean.
+# Our Hormonal condition is right to motivate our body.
+# If you practices this before Full Moon Your Body will be Supportive.
+# After Full Moon Its not Ideal Body Condition to Do Such Thing.
+
+print("\nIts your choice to decide what you will do bring a solution or do a job.")
+
+
+
+
