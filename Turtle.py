a3b37560804c5a5be35f4633a439ce1cb6dce556
sushen <sushenbiswasaga@gmail.com>
2018-11-06 11:04:14 +0600

    Language Vocabulary

---
+++ Turtle.py
@@ -0,0 +1,25 @@
+import turtle
+
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+#creat the turtle brade - draw a square
+    brad = turtle.Turtle()
+
+    brad.shape("turtle")
+    brad.color("White")
+    brad.speed(2)
+
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
+
+    window.exitonclick()
+
+draw_art()
